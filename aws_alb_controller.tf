f0d2afe316339397b1a9c1c39bd4337ee1b8ed50
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2024-04-01 14:19:42 +0200

    Update aws_alb_controller.tf

--- aws_alb_controller.tf
+++ aws_alb_controller.tf
@@ -1,4 +1,4 @@
-module "alb_controller" {
+module "aws_alb_controller" {
   source                              = "github.com/mohamedezz96/Terraform-Modules/EKS-Tools/AWS-ALB-Controller"
   aws_alb_controller_version          = "1.7.2"
   eks_issuer                          = data.aws_eks_cluster.eks_data.identity[0].oidc[0].issuer
