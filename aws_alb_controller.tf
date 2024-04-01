7ee20495184ef01728fd9f919c8e63966271da68
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2024-04-01 12:42:06 +0200

    Update aws_alb_controller.tf

--- aws_alb_controller.tf
+++ aws_alb_controller.tf
@@ -1,10 +1,10 @@
 module "alb_controller" {
-  source           = "github.com/mohamedezz96/Terraform-Modules/EKS-Tools/AWS-ALB-Controller"
-  version          = "1.7.2"
-  eks_issuer       = data.aws_eks_cluster.eks_data.identity[0].oidc[0].issuer
-  cluster_name     = "Testing" 
-  alb_policy_name  = "aws-alb-controller-policy"
-  alb_role_name    = "aws-alb-controller-role"
-  alb_sa_name      = "aws-alb-controller-serviceaccount"
-  values_file      = "./values/alb_controller.yaml"
+  source                              = "github.com/mohamedezz96/Terraform-Modules/EKS-Tools/AWS-ALB-Controller"
+  aws_alb_controller_version          = "1.7.2"
+  eks_issuer                          = data.aws_eks_cluster.eks_data.identity[0].oidc[0].issuer
+  cluster_name                        = "Testing" 
+  alb_policy_name                     = "aws-alb-controller-policy"
+  alb_role_name                       = "aws-alb-controller-role"
+  alb_sa_name                         = "aws-alb-controller-serviceaccount"
+  values_file                         = "./values/alb_controller.yaml"
 }
